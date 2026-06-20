95bbbf1d5a41e14a378a3f8aba87afe64c3d25c1
Arunima <145380571+arunimasharma33@users.noreply.github.com>
2026-06-20 19:00:12 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n= s.length();
+        if(n==0) return 0;
+        if(n==1){
+            if(s.charAt(0)!='0') return 1;
+            else return 0;
+        }
+        int dp[]= new int[n+1];
+        dp[0]=1;
+        if(s.charAt(0)!='0'){
+            dp[1]=1;
+        }else dp[1]=0;
+        for(int i=2; i<=n; i++){
+            if(s.charAt(i-1)!='0'){
+                dp[i]+=dp[i-1];
+            }
+            int twoDig= Integer.parseInt(s.substring(i-2, i));
+            if(twoDig>=10 && twoDig<=26){
+                dp[i]+=dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
