e12a362867f9bac992d018f96cd9c32010c3860b
Arunima <145380571+arunimasharma33@users.noreply.github.com>
2026-06-27 23:59:49 +0530

    Bulk sync: 60 submissions

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-0.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return dfs(root, root.val);
+    }
+    public int dfs(TreeNode root, int maxSeen){
+        if(root == null) return 0;
+
+        int good=0;
+        if(root.val >= maxSeen){
+            good++;
+        }
+        maxSeen= Math.max(maxSeen, root.val);
+
+        good+=dfs(root.left, maxSeen);
+        good+=dfs(root.right, maxSeen);
+
+        return good;
+
+    }
+}
